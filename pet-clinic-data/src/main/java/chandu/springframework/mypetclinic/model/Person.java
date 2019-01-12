46ab9b5e38fef5453540a1381b9c181237421447
chandu3015 <chandu3015@gmail.com>
2019-01-12 04:00:28 -0600

    Add id to model objects . Close #10

--- pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package chandu.springframework.mypetclinic.model;
 
-public class Person {
+public class Person extends  BaseEntity{
     private String firstName;
     private String LastName;
 
--- pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package chandu.springframework.mypetclinic.model;
 
-public class PetType {
+public class PetType extends  BaseEntity{
 
     private String Name;
 
