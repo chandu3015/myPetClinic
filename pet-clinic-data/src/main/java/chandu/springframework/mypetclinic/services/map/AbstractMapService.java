537a8b165fd2ab77cca6ff8cb311cf84303dce2d
chandu3015 <chandu3015@gmail.com>
2019-01-14 12:20:37 -0600

    Update Map Implementation to manage setting of ID property . Close #23

--- pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/chandu/springframework/mypetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package chandu.springframework.mypetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import chandu.springframework.mypetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll()
     {
@@ -20,7 +19,22 @@ public abstract class AbstractMapService<T,ID> {
 
     T save(ID id, T object)
     {
-        map.put(id,object);
+
+        if(object != null)
+        {
+            if(object.getId()==null)
+            {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(),object);
+        }
+        else
+        {
+            throw new RuntimeException("Object Cannot be NULL!!");
+        }
+
+
         return object;
     }
 
@@ -32,5 +46,20 @@ public abstract class AbstractMapService<T,ID> {
     void delete(T object)
     {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId()
+    {
+        Long nextId = null;
+
+        try
+        {
+           nextId = Collections.max(map.keySet())+1;
+        }
+        catch(NoSuchElementException e)
+        {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/chandu/springframework/mypetclinic/bootStrap/DataLoader.java
+++ pet-clinic-web/src/main/java/chandu/springframework/mypetclinic/bootStrap/DataLoader.java
@@ -26,13 +26,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("chandu");
         owner1.setLastName("konuru");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Raghu");
         owner2.setLastName("Kamamabti");
         ownerService.save(owner2);
@@ -40,13 +38,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
         vetService.save(vet2);
