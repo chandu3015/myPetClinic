52014eeca30c855686fd7c8b2cd46d18eced98d1
chandu3015 <chandu3015@gmail.com>
2019-01-13 19:40:56 -0600

    Adding Index Page and Controller . Close #8

---
+++ pet-clinic-web/src/main/java/chandu/springframework/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package chandu.springframework.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String Index()
+    {
+        return "index";
+    }
+}
