e43b2865f83505de3dc4425505efd07c0d037eae
chandu3015 <chandu3015@gmail.com>
2019-01-15 17:36:09 -0600

    Load pet type data. Close #32

--- pet-clinic-web/src/main/java/chandu/springframework/mypetclinic/bootStrap/DataLoader.java
+++ pet-clinic-web/src/main/java/chandu/springframework/mypetclinic/bootStrap/DataLoader.java
@@ -1,8 +1,10 @@
 package chandu.springframework.mypetclinic.bootStrap;
 
 import chandu.springframework.mypetclinic.model.Owner;
+import chandu.springframework.mypetclinic.model.PetType;
 import chandu.springframework.mypetclinic.model.Vet;
 import chandu.springframework.mypetclinic.services.OwnerService;
+import chandu.springframework.mypetclinic.services.PetTypeService;
 import chandu.springframework.mypetclinic.services.VetService;
 import chandu.springframework.mypetclinic.services.map.OwnerServiceMap;
 import chandu.springframework.mypetclinic.services.map.VetServiceMap;
@@ -14,16 +16,29 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private  final VetService vetService;
+    private  final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog= new PetType();
+        dog.setName("Dog");
+        PetType saveDogType = petTypeService.save(dog);
+
+        PetType cat= new PetType();
+        cat.setName("Cat");
+        PetType saveCatType = petTypeService.save(cat);
+
+        System.out.println("Loaded Pet Services");
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("chandu");
